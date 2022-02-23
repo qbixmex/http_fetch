c89868dafa3060d7b7f0883c65eef6773e9d8069
Daniel González Briseño <daniel.gb@globant.com>
2022-02-23 00:01:29 -0600

    Avoiding Callback Hell

--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@ import "./sass/styles.scss";
 
 const jokeUrl = "https://api.chucknorris.io/jokes/random";
 
-fetch( jokeUrl ).then( response => {
-    response.json().then( ({ id, value }) => {
+// fetch( jokeUrl ).then( response => {
+//     response.json().then( ({ id, value }) => {
+//         console.log( "ID:", id );
+//         console.log( "Value:", value );
+//     })
+// });
+
+fetch( jokeUrl )
+    .then( response => response.json() )
+    .then( ({ id, value }) => {
         console.log( "ID:", id );
         console.log( "Value:", value );
-    })
-});
+    });
