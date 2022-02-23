7936b6d5e51748003972180a072e778222000dea
Daniel González Briseño <daniel.gb@globant.com>
2022-02-22 23:57:20 -0600

    Fetching Data

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import "./sass/styles.scss";
 
-console.log( "Hello World" );
+const jokeUrl = "https://api.chucknorris.io/jokes/random";
+
+fetch( jokeUrl ).then( response => {
+    response.json().then( ({ id, value }) => {
+        console.log( "ID:", id );
+        console.log( "Value:", value );
+    })
+});
